102b664b6e919504f9041e7d4b9ece69bea6ceac
JairoRiver <jairo.river@protonmail.com>
2024-08-17 18:31:59 +0200

    Add Aux sKey Postgres repository Update method

--- short_link/internal/repository/db/postgres/aux_s_key.go
+++ short_link/internal/repository/db/postgres/aux_s_key.go
@@ -6,6 +6,7 @@ import (
 	"github.com/JairoRiver/short_link_app/short_link/internal/repository"
 	"github.com/JairoRiver/short_link_app/short_link/pkg/model"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 // GetAuxSKey retrieves data to generate S Keys.
@@ -29,6 +30,37 @@ func (q *Queries) GetAuxSKey(ctx context.Context) (*model.AuxSKey, error) {
 	if err == pgx.ErrNoRows {
 		err = fmt.Errorf("Repository postgres GetAuxSKey method error: %w", repository.ErrNotFound)
 	}
+
+	return &i, err
+}
+
+// UpdateAuxSKey retrieves data to generate S Keys.
+const updateAuxSKeyQuery = `
+UPDATE aux_s_key
+SET A0 = COALESCE($1, A0), 
+    N0 = COALESCE($2, N0)
+RETURNING N, "End", Step, A0, N0
+`
+
+type updateAuxSKeyParams struct {
+	A0 pgtype.Int8
+	N0 pgtype.Int8
+}
+
+func (q *Queries) UpdateAuxSKey(ctx context.Context, params repository.AuxSKeyParams) (*model.AuxSKey, error) {
+	args := updateAuxSKeyParams{
+		A0: pgtype.Int8{Int64: int64(params.A0.Value), Valid: params.A0.Valid},
+		N0: pgtype.Int8{Int64: int64(params.N0.Value), Valid: params.N0.Valid},
+	}
+	row := q.db.QueryRow(ctx, updateAuxSKeyQuery, args.A0, args.N0)
+	var i model.AuxSKey
+	err := row.Scan(
+		&i.N,
+		&i.End,
+		&i.Step,
+		&i.A0,
+		&i.N0,
+	)
 
 	return &i, err
 }
--- short_link/internal/repository/db/postgres/aux_s_key_test.go
+++ short_link/internal/repository/db/postgres/aux_s_key_test.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"github.com/JairoRiver/short_link_app/short_link/internal/repository"
 	"github.com/JairoRiver/short_link_app/short_link/internal/util"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -15,4 +16,18 @@ func TestGetAuxSKey(t *testing.T) {
 	assert.Equal(t, auxSkey.Step, uint(util.SkstepValue))
 	assert.Equal(t, auxSkey.End, uint(util.SkendValue))
 	assert.NoError(t, err)
+}
+
+func TestUpdateAuxSKey(t *testing.T) {
+	// Test update skAux
+	params := repository.AuxSKeyParams{
+		A0: repository.IsIntValid{Valid: true, Value: uint(util.RandomInt(minRandomSk, maxRandomSk))},
+		N0: repository.IsIntValid{Valid: true, Value: uint(util.RandomInt(minRandomSk, maxRandomSk))},
+	}
+	deletedLink, err := testStore.UpdateAuxSKey(context.Background(), params)
+	assert.NotNil(t, deletedLink)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, deletedLink)
+	assert.Equal(t, deletedLink.A0, params.A0.Value)
+	assert.Equal(t, deletedLink.N0, params.N0.Value)
 }
