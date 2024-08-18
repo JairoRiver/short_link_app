9aa38e3b8df682591aa610211cf3247bfc20732c
JairoRiver <jairo.river@protonmail.com>
2024-08-18 14:07:28 +0200

    Add a gin production mode

--- short_link/cmd/api/main.go
+++ short_link/cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"github.com/JairoRiver/short_link_app/short_link/internal/api"
 	"github.com/JairoRiver/short_link_app/short_link/internal/api/handler/rest"
 	"github.com/JairoRiver/short_link_app/short_link/internal/controller"
+	"github.com/JairoRiver/short_link_app/short_link/internal/repository"
+	"github.com/JairoRiver/short_link_app/short_link/internal/repository/db/postgres"
 	"github.com/JairoRiver/short_link_app/short_link/internal/repository/memory"
 	"github.com/JairoRiver/short_link_app/short_link/internal/util"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
@@ -18,13 +22,19 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Msg("cannot load config")
 	}
-
+	var repo repository.Storer
 	if config.Environment == "development" {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+		repo = memory.New()
+	} else {
+		connPool, err := pgxpool.New(context.Background(), config.DBSource)
+		if err != nil {
+			log.Fatal().Err(err).Msg("cannot connect to db")
+		}
+		repo = postgres.New(connPool)
 	}
 
-	repo := memory.New()
 	control := controller.New(repo)
 	handler := rest.New(control, config)
 
